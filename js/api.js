4c76ed196c09a8d708694c5b8b8f8384d6f5e176
HIENPHAM <phamthehien120@gmail.com>
2023-08-11 21:55:23 +0700

    login/register

---
+++ js/api.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $('#loginForm').submit(function (event) {
+        event.preventDefault();
+        const email = $('#email').val();
+        const password = $('#password').val();
+        const loginEndpoint = 'https://api.tipslife.site/api/User/Login';
+        const loginData = {
+            email: email,
+            password: password
+        };
+        console.log(loginData);
+        $.ajax({
+            type: 'POST',
+            url: loginEndpoint,
+            data: JSON.stringify(loginData),
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function (response, textStatus, xhr) {
+                if (xhr.status === 200) {
+                    console.log('Response:', xhr);
+                } else {
+                    $('#loginStatus').text('Login failed. Please check your credentials.');
+                    console.log('Login failed. Status code:', xhr.status);
+                }
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                $('#loginStatus').text('An error occurred during login. Please try again later.');
+                console.error('An error occurred during login. Status code:', xhr.status);
+            }
+
+        });
+    });
+});
---
+++ js/settings.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    const signUpButton = $('#signUp');
+    const signInButton = $('#signIn');
+    const container = $('#container');
+  
+    signUpButton.click(function() {
+      container.addClass("right-panel-active");
+    });
+  
+    signInButton.click(function() {
+      container.removeClass("right-panel-active");
+    });
+  });
+  
