aa91e9f06d10615ed62603616b20740be2719503
HIENPHAM <phamthehien120@gmail.com>
2023-08-13 21:44:08 +0700

    update ui

--- js/api.js
+++ js/api.js
@@ -29,18 +29,20 @@ $(document).ready(function () {
                     console.log('Access Token:', accessToken);
                     localStorage.setItem('accessToken', accessToken);
                     window.location.href = 'user.html';
+                } else if (xhr.status === 401) {
+                    window.location.href = 'index.html';
                 } else {
                     $('#loginStatus').text('Login failed. Please check your credentials.');
                     console.log('Login failed. Status code:', xhr.status);
                 }
-                setTimeout(function() {
+                setTimeout(function () {
                     $('#loginStatus').hide();
                 }, 5000);
             },
             error: function (xhr, textStatus, errorThrown) {
                 $('#loginStatus').text('An error occurred during login. Please try again later.');
                 console.error('An error occurred during login. Status code:', xhr.status);
-                setTimeout(function() {
+                setTimeout(function () {
                     $('#loginStatus').hide();
                 }, 5000);
             }
@@ -53,15 +55,34 @@ $(document).ready(function () {
         headers: {
             "Authorization": "Bearer " + accessToken
         },
-        success: function(users) {
+        success: function (users) {
             console.log("API response:", users);
+            if (users.length > 0) {
+                var firstUser = users[0];
+                var userName = firstUser.userName;
+                localStorage.setItem("userName", userName);
+            } else {
+                console.log("No users found in the response.");
+            }
+            var storedUserName = localStorage.getItem("userName");
+            var uNameElement = $("#uName");
+            if (uNameElement.length) {
+                if (storedUserName) {
+                    uNameElement.text(storedUserName);
+                } else {
+                    console.log("No stored userName found.");
+                }
+            } else {
+                console.error("#uName element not found.");
+            }
+
             var userTableBody = $("#user-table-body");
             if (!userTableBody.length) {
                 console.error("#user-table-body element not found.");
                 return;
             }
             userTableBody.empty();
-            $.each(users, function(index, user) {
+            $.each(users, function (index, user) {
                 var row = $("<tr>");
                 row.append($("<th scope='row'>").text(user.userID));
                 row.append($("<td>").text(user.userName));
@@ -74,12 +95,11 @@ $(document).ready(function () {
                 var minusButton = $("<button>").text("-");
                 minusButton.addClass("btn btn-danger minus-button");
                 row.append($("<td>").append(minusButton));
-                
+
                 userTableBody.append(row);
             });
-            
         },
-        error: function(jqXHR, textStatus, errorThrown) {
+        error: function (jqXHR, textStatus, errorThrown) {
             console.error("API request failed:", textStatus, errorThrown);
         }
     });
--- js/settings.js
+++ js/settings.js
@@ -1,29 +1,45 @@
-$(document).ready(function() {
-    const signUpButton = $('#signUp');
-    const signInButton = $('#signIn');
-    const container = $('#container');
-  
-    signUpButton.click(function() {
-      container.addClass("right-panel-active");
-    });
-  
-    signInButton.click(function() {
-      container.removeClass("right-panel-active");
-    });
+$(document).ready(function () {
+  const signUpButton = $('#signUp');
+  const signInButton = $('#signIn');
+  const container = $('#container');
 
-    var menuicn = $(".menuicn");
-    var nav = $(".navcontainer");
-  
-    menuicn.click(function() {
-      nav.toggleClass("navclose");
-    });
-
-    $('#logoutButton').on('click', function() {
-      localStorage.removeItem('accessToken');
-      
-      window.location.href = 'index.html';
-  });
+  signUpButton.click(function () {
+    container.addClass("right-panel-active");
   });
 
-  
-  
+  signInButton.click(function () {
+    container.removeClass("right-panel-active");
+  });
+
+  var menuicn = $(".menuicn");
+  var nav = $(".navcontainer");
+
+  menuicn.click(function () {
+    nav.toggleClass("navclose");
+  });
+
+  $('#logoutButton').on('click', function () {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('userName');
+    window.location.href = 'index.html';
+  });
+
+  function toggleMode() {
+    const isDarkMode = $('body').hasClass('dark-mode');
+    $('body').toggleClass('dark-mode', !isDarkMode);
+    $('.header').toggleClass('dark-mode', !isDarkMode);
+  }
+  $('#toggleMode').on('click', toggleMode);
+
+  var storedUserName = localStorage.getItem("userName");
+  var uNameElement = $("#uName");
+  if (uNameElement.length) {
+    uNameElement.text(storedUserName);
+  } else {
+    console.error("#uName element not found.");
+  }
+
+});
+
+
+
