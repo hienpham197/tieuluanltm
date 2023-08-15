52534a8c8f852d1b8ab84af7b1ea60bd665fdccb
Heran <hung.tran2704@hcmut.edu.vn>
2023-08-15 16:39:47 +0700

    Delete, Edit, Add

--- js/api.js
+++ js/api.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
     var accessToken = localStorage.getItem('accessToken');
-    var currentPage = 0;
-    var itemsPerPage = 10;
-    var totalPages = 3;
-
     $('#loginForm').submit(function (event) {
         event.preventDefault();
         const email = $('#email').val();
@@ -53,6 +49,65 @@ $(document).ready(function () {
         });
     });
 
+    // $.ajax({
+    //     url: "https://api.tipslife.site/api/User/GetListUser",
+    //     method: "GET",
+    //     headers: {
+    //         "Authorization": "Bearer " + accessToken
+    //     },
+    //     success: function (users) {
+    //         console.log("API response:", users);
+    //         if (users.length > 0) {
+    //             var firstUser = users[0];
+    //             var userName = firstUser.userName;
+    //             localStorage.setItem("userName", userName);
+    //         } else {
+    //             console.log("No users found in the response.");
+    //         }
+    //         var storedUserName = localStorage.getItem("userName");
+    //         var uNameElement = $("#uName");
+    //         if (uNameElement.length) {
+    //             if (storedUserName) {
+    //                 uNameElement.text(storedUserName);
+    //             } else {
+    //                 console.log("No stored userName found.");
+    //             }
+    //         } else {
+    //             console.error("#uName element not found.");
+    //         }
+
+    //         var userTableBody = $("#user-table-body");
+    //         if (!userTableBody.length) {
+    //             console.error("#user-table-body element not found.");
+    //             return;
+    //         }
+    //         userTableBody.empty();
+    //         $.each(users, function (index, user) {
+    //             var row = $("<tr>");
+    //             row.append($("<th scope='row'>").text(user.userID));
+    //             row.append($("<td>").text(user.userName));
+    //             row.append($("<td>").text(user.firstName));
+    //             row.append($("<td>").text(user.lastName));
+    //             row.append($("<td>").text(user.email));
+    //             var plusButton = $("<button>").text("+");
+    //             plusButton.addClass("btn btn-success plus-button");
+    //             row.append($("<td>").append(plusButton));
+    //             var minusButton = $("<button>").text("-");
+    //             minusButton.addClass("btn btn-danger minus-button");
+    //             row.append($("<td>").append(minusButton));
+    //             userTableBody.append(row);
+    //         });
+    //     },
+    //     error: function (jqXHR, textStatus, errorThrown) {
+    //         console.error("API request failed:", textStatus, errorThrown);
+    //     }
+    // });
+
+
+
+    var currentPage = 0;
+    var itemsPerPage = 10;
+
     function displayUsersOnPage(users) {
         var userTableBody = $("#user-table-body");
         userTableBody.empty();
@@ -63,30 +118,178 @@ $(document).ready(function () {
             row.append($("<td>").text(user.firstName));
             row.append($("<td>").text(user.lastName));
             row.append($("<td>").text(user.email));
-            var plusButton = $("<button>").text("+");
-            plusButton.addClass("btn btn-success plus-button");
-            row.append($("<td>").append(plusButton));
-            var minusButton = $("<button>").text("-");
-            minusButton.addClass("btn btn-danger minus-button");
-            row.append($("<td>").append(minusButton));
+
+            var editButton = $("<button>").text("+");
+            editButton.addClass("btn btn-primary edit-button");
+            editButton.data("user-id", user.userID);
+            row.append($("<td>").append(editButton));
+
+            var deleteButton = $("<button>").text("-");
+            deleteButton.addClass("btn btn-danger delete-button");
+            deleteButton.data("user-id", user.userID);
+            row.append($("<td>").append(deleteButton));
 
             userTableBody.append(row);
         });
     }
+    //  Delete ///////////////////////////////////////////
+    $(document).on("click", ".delete-button", function () {
+        var userId = $(this).data("user-id");
+        if (confirm("Are you sure you want to delete this user?")) {
+            $.ajax({
+                url: "https://api2.tipslife.site/api/User/Delete/" + userId,
+                method: "DELETE",
+                headers: {
+                    "Authorization": "Bearer " + accessToken
+                },
+                success: function () {
+                    fetchDataAndUpdateTable();
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    console.error("Delete request failed:", textStatus, errorThrown);
+                }
+            });
+        }
+    });
+
+    // Edit //////////////////////////////////////////////////
+
+    $(document).on("click", ".edit-button", function () {
+        var userId = $(this).data("user-id");
+        var row = $(this).closest("tr");
+        
+        var userNameInput = $("<input>").val(row.find("td:eq(0)").text());
+        var firstNameInput = $("<input>").val(row.find("td:eq(1)").text());
+        var lastNameInput = $("<input>").val(row.find("td:eq(2)").text());
+        var emailInput = $("<input>").val(row.find("td:eq(3)").text());
+        
+        row.find("td:eq(0)").html(userNameInput);
+        row.find("td:eq(1)").html(firstNameInput);
+        row.find("td:eq(2)").html(lastNameInput);
+        row.find("td:eq(3)").html(emailInput);
+        
+        var saveButton = $("<button>").text("Save");
+        saveButton.addClass("btn btn-success save-button");
+        row.find("td:eq(4)").html(saveButton);
+        
+        var cancelButton = $("<button>").text("Cancel");
+        cancelButton.addClass("btn btn-secondary cancel-button");
+        row.find("td:eq(5)").html(cancelButton);
+    });
+
+    $(document).on("click", ".save-button", function () {
+        var userId = $(this).closest("tr").find(".edit-button").data("user-id");
+        var userName = $(this).closest("tr").find("input:eq(0)").val();
+        var firstName = $(this).closest("tr").find("input:eq(1)").val();
+        var lastName = $(this).closest("tr").find("input:eq(2)").val();
+        var email = $(this).closest("tr").find("input:eq(3)").val();
+        
+        var userData = {
+            userID: userId,
+            userName: userName,
+            firstName: firstName,
+            lastName: lastName,
+            email: email
+        };
+        
+        $.ajax({
+            url: "https://api2.tipslife.site/api/User/Update/" + userId,
+            method: "PUT",
+            data: JSON.stringify(userData),
+            contentType: "application/json",
+            headers: {
+                "Authorization": "Bearer " + accessToken
+            },
+            success: function () {
+                fetchDataAndUpdateTable();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error("Update request failed:", textStatus, errorThrown);
+            }
+        });
+    });
+
+            $(document).on("click", ".cancel-button", function () {
+                fetchDataAndUpdateTable();
+    });
+
+            // Add user ///////////////////////////////////////
+
+        $("#add-user-button").on("click", function () {
+        var newUser = {
+            userID: null,
+            userName: "",
+            firstName: "",
+            lastName: "",
+            email: ""
+        };
+        
+        var row = $("<tr>");
+        row.append($("<th scope='row'>").text(""));
+        row.append($("<td>").append($("<input>").val(newUser.userName).addClass("new-user-input")));
+        row.append($("<td>").append($("<input>").val(newUser.firstName).addClass("new-user-input")));
+        row.append($("<td>").append($("<input>").val(newUser.lastName).addClass("new-user-input")));
+        row.append($("<td>").append($("<input>").val(newUser.email).addClass("new-user-input")));
+
+        var saveButton = $("<button>").text("Save");
+        saveButton.addClass("btn btn-success save-new-button");
+        row.append($("<td>").append(saveButton));
+
+        var cancelButton = $("<button>").text("Cancel");
+        cancelButton.addClass("btn btn-secondary cancel-new-button");
+        row.append($("<td>").append(cancelButton));
+
+        $("#user-table-body").prepend(row);
+    });
+
+
+       $(document).on("click", ".save-new-button", function () {
+        var row = $(this).closest("tr");
+        var userName = row.find(".new-user-input:eq(0)").val();
+        var firstName = row.find(".new-user-input:eq(1)").val();
+        var lastName = row.find(".new-user-input:eq(2)").val();
+        var email = row.find(".new-user-input:eq(3)").val();
+
+        var newUser = {
+            userName: userName,
+            firstName: firstName,
+            lastName: lastName,
+            email: email
+        };
+
+        $.ajax({
+            url: "https://api2.tipslife.site/api/User/Register",
+            method: "POST",
+            data: JSON.stringify(newUser),
+            contentType: "application/json",
+            headers: {
+                "Authorization": "Bearer " + accessToken
+            },
+            success: function () {
+                fetchDataAndUpdateTable();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error("Add new user request failed:", textStatus, errorThrown);
+            }
+        });
+    });
+
+    $(document).on("click", ".cancel-new-button", function () {
+        $(this).closest("tr").remove();
+    });
+
 
     function updatePaginationButtons(totalPages) {
-        var paginationContainer = $(".pagination");
+        var paginationContainer = $("#pagination-container");
         paginationContainer.empty();
 
-        var previousButton = $("<li class='page-item' id='previous-page'>")
-            .append($("<a class='page-link' href='#' tabindex='-1'>").text("Previous"));
-
-        if (currentPage === 0) {
+        var previousButton = $("<button>").text("Previous");
+        previousButton.addClass("btn btn-secondary page-button");
+        if (currentPage === 0) { // Adjusted comparison for first page
             previousButton.addClass("disabled");
         } else {
             previousButton.on("click", function () {
                 currentPage--;
-                updatePageState();
                 fetchDataAndUpdateTable();
             });
         }
@@ -94,42 +297,50 @@ $(document).ready(function () {
         paginationContainer.append(previousButton);
 
         for (var i = 1; i <= totalPages; i++) {
-            var pageButton = $("<li class='page-item'>")
-                .append($("<a class='page-link' href='#'>").text(i));
-
-            if (i === currentPage + 1) {
+            var pageButton = $("<button>").text(i);
+            pageButton.addClass("btn btn-secondary page-button");
+            if (i === currentPage + 1) { // Adjusted comparison with offset
                 pageButton.addClass("active");
             }
-
             pageButton.on("click", function () {
-                currentPage = parseInt($(this).text()) - 1;
-                updatePageState();
+                currentPage = parseInt($(this).text()) - 1; // Adjusted page assignment
                 fetchDataAndUpdateTable();
             });
-
             paginationContainer.append(pageButton);
         }
 
-        var nextButton = $("<li class='page-item' id='next-page'>")
-            .append($("<a class='page-link' href='#'>").text("Next"));
-
-        if (currentPage === totalPages - 1) {
+        var nextButton = $("<button>").text("Next");
+        nextButton.addClass("btn btn-secondary page-button");
+        if (currentPage === totalPages - 1) { // Adjusted comparison for last page
             nextButton.addClass("disabled");
         } else {
             nextButton.on("click", function () {
                 currentPage++;
-                updatePageState();
                 fetchDataAndUpdateTable();
             });
         }
 
         paginationContainer.append(nextButton);
     }
-
-    function updatePageState() {
-        history.pushState(null, null, "?page=" + (currentPage + 1));
-    }
+    $("#next-page").on("click", function () {
+        currentPage++;
+        fetchDataAndUpdateTable();
+    });
     
+    $("#previous-page").on("click", function () {
+        currentPage--;
+        fetchDataAndUpdateTable();
+    });
+    
+    $(document).on("click", ".page-button a", function () {
+        console.log("Page button clicked");
+        currentPage = parseInt($(this).text()) - 1;
+        console.log("currentPage:", currentPage);
+        fetchDataAndUpdateTable();
+    });
+    
+    
+
     function fetchDataAndUpdateTable() {
         var apiUrl = "https://api2.tipslife.site/api/User/GetListUser";
         var pageNumber = currentPage;
@@ -153,4 +364,5 @@ $(document).ready(function () {
     }
 
     fetchDataAndUpdateTable();
+
 });
